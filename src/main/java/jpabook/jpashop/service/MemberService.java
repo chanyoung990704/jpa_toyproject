10d76f9bf8ed2a2b0f425b6d5737ce43738c0340
CYPARK <chanyoung990704@naver.com>
2023-07-26 18:24:28 +0900

    add Member repository and service

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,60 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+
+        memberRepository.saveMember(member);
+
+        return member.getId();
+
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+
+
+    private void validateDuplicateMember(Member member) {
+
+        String curName = member.getName();
+        List<Member>nameList = memberRepository.findByName(curName);
+
+        if (!nameList.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원");
+        }
+
+
+
+
+
+    }
+
+
+}
