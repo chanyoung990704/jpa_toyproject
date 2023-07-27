d0a0d0c8c3e9e2742d40ec6078989c5925eb2c0c
CYPARK <chanyoung990704@naver.com>
2023-07-27 16:34:48 +0900

    import querydsl to build.gradle

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,12 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.Query;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -12,10 +18,12 @@ public class OrderRepository {
 
     @PersistenceContext
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
 
 
     public OrderRepository(EntityManager em) {
         this.em = em;
+        queryFactory = new JPAQueryFactory(em);
     }
 
 
@@ -29,9 +37,23 @@ public class OrderRepository {
     }
 
 
-/*    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAll(OrderSearch orderSearch) {
 
-    }*/
+        QMember member = QMember.member;
+        QOrder order = QOrder.order;
+        BooleanExpression predicate = order.status.eq(orderSearch.getOrderStatus());
+
+        if (orderSearch.getMemberName() != null && !orderSearch.getMemberName().isEmpty()) {
+            predicate = predicate.and(order.member.name.eq(orderSearch.getMemberName()));
+        }
+
+        return queryFactory
+                .selectFrom(order)
+                .join(order.member, member)
+                .where(predicate)
+                .fetch();
+
+    }
 
 
 }
