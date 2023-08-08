880f6ba3d92fe4916cc222ab82afebb781549832
CYPARK <chanyoung990704@naver.com>
2023-08-08 13:23:52 +0900

    add orderCancel

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -11,10 +11,7 @@ import jpabook.jpashop.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -74,6 +71,14 @@ public class OrderController {
         return "order/orderList";
 
 
+    }
+
+
+    @PostMapping("/orders/{id}/cancel")
+    public String orderCancel(@PathVariable(name = "id") Long orderId)
+    {
+        orderService.cancel(orderId);
+        return "redirect:/";
     }
 
 
